28c132d06a2c6cd6b160872234bf28980e8ad312
Ashraf-Rahil <skashraf97@gmail.com>
2023-01-18 22:21:36 +0530

    added browerslist in package.json

---
+++ dist/index.41569fbf.js
@@ -0,0 +1,2 @@
+const heading=React.createElement("h1",{id:"title"},"Heading 1");console.log(heading);const root=ReactDOM.createRoot(document.getElementById("root"));root.render(heading);
+//# sourceMappingURL=index.41569fbf.js.map
